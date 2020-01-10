63914684dd7005c96a1838be4b4b6c34cf9c2bee
Hadi El-Yakhni <57732964+Hadi-El-Yakhni@users.noreply.github.com>
2020-01-10 20:02:23 +0200

    trying to fix user signup

--- src/routers/user.js
+++ src/routers/user.js
@@ -7,115 +7,115 @@ const auth = require('../middlewares/auth')
 const router = new express.Router()
 
 router.post('/users/signup', async (req, res) => {
-  const user = new User(req.body)
-  try {
-    await user.save()
-    const token = await user.generateAuthToken()
-    res.status(201).send({ user, token })
-  } catch (e) {
-    res.status(400).send(e)
-  }
+    const user = new User(req.body)
+    try {
+        await user.save()
+        const token = await user.generateAuthToken()
+        res.status(201).send({ user, token })
+    } catch (e) {
+        res.status(400).send('walla ma mishi l hal')
+    }
 })
 
 router.post('/users/login', async (req, res) => {
-  try {
-    const user = await User.findByCredentials(req.body.email, req.body.password)
-    const token = await user.generateAuthToken()
-    res.send({ user, token })
-  } catch (e) {
-    res.status(404).send()
-  }
+    try {
+        const user = await User.findByCredentials(req.body.email, req.body.password)
+        const token = await user.generateAuthToken()
+        res.send({ user, token })
+    } catch (e) {
+        res.status(404).send()
+    }
 })
 
 router.get('/users/logout', auth, async (req, res) => {
-  try {
-    req.user.tokens = req.user.tokens.filter((token) => {
-      return token.token !== req.token
-    })
-    await req.user.save()
-    res.send()
-  } catch (e) {
-    res.status(500).send()
-  }
+    try {
+        req.user.tokens = req.user.tokens.filter((token) => {
+            return token.token !== req.token
+        })
+        await req.user.save()
+        res.send()
+    } catch (e) {
+        res.status(500).send()
+    }
 })
 
 router.post('/users/logoutAll', auth, async (req, res) => {
-  try {
-    req.user.tokens = []
-    await req.user.save()
-    res.send()
-  } catch (e) {
-    res.status(500).send()
-  }
+    try {
+        req.user.tokens = []
+        await req.user.save()
+        res.send()
+    } catch (e) {
+        res.status(500).send()
+    }
 })
 
 router.get('/users/me', auth, async (req, res) => {
-  res.send(req.user)
+    res.send(req.user)
 })
 
 const upload = multer({
-  limits: {
-    fileSize: 1000000   // 1 MB,  
-  },
-  fileFilter(req, file, cb) {
-    // cb(new Error('Error ...'))   ==> if error
-    // cb(undefined, true)          ==> if upload should be expected
-    // cb(undefined, false)         ==> if upload should be rejected  (will not use)
-    if (!file.originalname.match(/\.(jpg|jpeg|png)$/))
-      return cb(new Error('Please upload an image.'))
-    return cb(undefined, true)
-  }
+    limits: {
+        fileSize: 1000000   // 1 MB,  
+    },
+    fileFilter(req, file, cb) {
+        // cb(new Error('Error ...'))   ==> if error
+        // cb(undefined, true)          ==> if upload should be expected
+        // cb(undefined, false)         ==> if upload should be rejected  (will not use)
+        if (!file.originalname.match(/\.(jpg|jpeg|png)$/))
+            return cb(new Error('Please upload an image.'))
+        return cb(undefined, true)
+    }
 })
 
 router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) => {
-  buffer = await sharp(req.file.buffer).resize({ width: 250, height: 250 }).png().toBuffer()
-  req.user.avatar = buffer
-  await req.user.save()
-  res.send()
+    buffer = await sharp(req.file.buffer).resize({ width: 250, height: 250 }).png().toBuffer()
+    req.user.avatar = buffer
+    await req.user.save()
+    res.send()
 }, (error, req, res, next) => {
-  res.status(400).send({ error: `${error.message}` })
+    res.status(400).send({ error: `${error.message}` })
 })
 
 router.delete('/users/me/avatar', auth, async (req, res) => {
-  req.user.avatar = undefined
-  await req.user.save()
-  res.send()
+    req.user.avatar = undefined
+    await req.user.save()
+    res.send()
 })
 
 router.get('/users/:id/avatar', async (req, res) => {
-  try {
-    const user = await User.findById(req.params.id)
-    if (!user || !user.avatar)
-      throw new Error()
-    res.set('content-type', 'image/png')
-    res.send(user.avatar)
-  } catch (error) {
-    res.status(404).send()
-  }
+    try {
+        const user = await User.findById(req.params.id)
+        if (!user || !user.avatar)
+            throw new Error()
+        res.set('content-type', 'image/png')
+        res.send(user.avatar)
+    } catch (error) {
+        res.status(404).send()
+    }
 })
 
 router.patch('/users/me', auth, async (req, res) => {
-  const updates = Object.keys(req.body)
-  const allowedUpdates = ['name', 'email', 'password', 'age']
-  const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
-  if (!isValidOperation)
-    return res.status(400).send({ error: 'Invalid updates!' })
-  try {
-    updates.forEach((update) => req.user[update] = req.body[update])
-    await req.user.save()
-    res.send(req.user)
-  } catch (e) {
-    res.status(400).send(e)
-  }
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['name', 'email', 'password', 'age']
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+    if (!isValidOperation)
+        return res.status(400).send({ error: 'Invalid updates!' })
+    try {
+        updates.forEach((update) => req.user[update] = req.body[update])
+        await req.user.save()
+        res.send(req.user)
+    } catch (e) {
+        res.status(400).send(e)
+    }
 })
 
 router.delete('/users/me', auth, async (req, res) => {
-  try {
-    await req.user.remove()
-    res.send(req.user)
-  } catch (e) {
-    res.status(500).send()
-  }
+    try {
+        await req.user.remove()
+        res.send(req.user)
+    } catch (e) {
+        res.status(500).send()
+    }
 })
 
 module.exports = router
